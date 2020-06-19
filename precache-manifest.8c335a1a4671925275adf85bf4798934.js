0d3d80844552813a50d9ccfe9a274c64e08d5014
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-19 09:12:36 +0500

    Updates

---
+++ precache-manifest.8c335a1a4671925275adf85bf4798934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a93be3fd2e04a76774c7a1b523ffb9",
+    "url": "/React-Test/index.html"
+  },
+  {
+    "revision": "5d0e7c41433eb31e88d0",
+    "url": "/React-Test/static/css/main.20f881e3.chunk.css"
+  },
+  {
+    "revision": "eca2b76bcf2df70e909b",
+    "url": "/React-Test/static/js/2.2e89d915.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Test/static/js/2.2e89d915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0e7c41433eb31e88d0",
+    "url": "/React-Test/static/js/main.46ff3655.chunk.js"
+  },
+  {
+    "revision": "d741f8d8d2fa262db9c7",
+    "url": "/React-Test/static/js/runtime-main.ea6b898d.js"
+  }
+]);
